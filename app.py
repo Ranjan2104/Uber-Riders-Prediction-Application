ff2cfced115897c17778a79d8df08ad7d1164dba
Amresh2104 <60054130+Ranjan2104@users.noreply.github.com>
2022-03-29 11:49:56 +0530

    initial coomit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import numpy as np
+import math
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def Home():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    op = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text = "Number of Weekly Riders is {}".format(math.floor(op)))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
